7615fb03a428423aed51e0ebc06fc29bc04b7b4d
Vivek <varora2004@gmail.com>
2024-11-01 19:43:55 +0530

    update

--- src/config/index.js
+++ src/config/index.js
@@ -145,38 +145,38 @@ export const shoppingViewHeaderMenuItems = [
 ];
 
 export const categoryOptionsMap = {
-  men: "Men",
-  women: "Women",
-  kids: "Kids",
-  accessories: "Accessories",
-  footwear: "Footwear",
+  Tshirts: "OverSized TShirts",
+  // women: "Women",
+  // kids: "Kids",
+  // accessories: "Accessories",
+  // footwear: "Footwear",
 };
 
-export const brandOptionsMap = {
-  nike: "Nike",
-  adidas: "Adidas",
-  puma: "Puma",
-  levi: "Levi",
-  zara: "Zara",
-  "h&m": "H&M",
-};
+// export const brandOptionsMap = {
+//   nike: "Nike",
+//   adidas: "Adidas",
+//   puma: "Puma",
+//   levi: "Levi",
+//   zara: "Zara",
+//   "h&m": "H&M",
+// };
 
 export const filterOptions = {
   category: [
-    { id: "men", label: "Men" },
+    { id: "Tshirts", label: "OverSized TShirts" },
     { id: "women", label: "Women" },
     { id: "kids", label: "Kids" },
     { id: "accessories", label: "Accessories" },
     { id: "footwear", label: "Footwear" },
   ],
-  brand: [
-    { id: "nike", label: "Nike" },
-    { id: "adidas", label: "Adidas" },
-    { id: "puma", label: "Puma" },
-    { id: "levi", label: "Levi's" },
-    { id: "zara", label: "Zara" },
-    { id: "h&m", label: "H&M" },
-  ],
+  // brand: [
+  //   { id: "nike", label: "Nike" },
+  //   { id: "adidas", label: "Adidas" },
+  //   { id: "puma", label: "Puma" },
+  //   { id: "levi", label: "Levi's" },
+  //   { id: "zara", label: "Zara" },
+  //   { id: "h&m", label: "H&M" },
+  // ],
 };
 
 export const sortOptions = [
